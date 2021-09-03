1f1a55fae12cd8ea2f2854bc580baeca74780ec0
rcaoagdan <rcaoagdan@icloud.com>
2021-09-02 17:50:49 -0700

    skeleton

--- hippadown.py
+++ hippadown.py
@@ -3,8 +3,8 @@
 ##############################################################################
 #                          !!!!!!DISCLAIMER!!!!!!                            # 
 ##############################################################################
-# The purpose of this code, is to demonstrate an attempted attack, with the goal 
-# of HIPPA Data Exfiltration.
+# The purpose of this code, is to demonstrate an attempted attack, with the 
+# goal of HIPPA Data Exfiltration.
 
 # All testing was done on a conrtolled enviorment to show a SIEM event being
 # triggered.
@@ -13,8 +13,10 @@
 
 # PLEASE DO NOT USE THIS CODE FOR NEFARIOUS PURPOSES.
 
+# We do not condone using this for malicious purpose, and are not responsible
+# if you are caught using our code for malicious purposes.
 ##############################################################################
-#                                                                            #
+#                               Dogma Inc.                                   #                                     
 ##############################################################################
 
 ##############################################################################
